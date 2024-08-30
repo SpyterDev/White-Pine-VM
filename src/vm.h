cfae4c31387745fadd3700b1a9f7e70ee0f10fa4
SpyterDev <spyterdev@gmail.com>
2024-08-30 01:14:30 -0400

    Updated vm header, deleted macros

--- src/vm.h
+++ src/vm.h
@@ -1,32 +1,6 @@
 #pragma once
 #include <stdint.h>
-
-// Important macros
-typedef uint8_t * arithmetic_void_ptr;
-#define allocated_memory 53248
-#define stack_size 524288
-#define less_than 1
-#define greater_than 2
-#define equal_to 4
-#define all_cond 16
-#define real_address_mask 0xfff
-#define real_address_bits 12
-#define stack_mem_pages stack_size >> real_address_bits
-#define heap_mem_pages allocated_memory / real_address_bits
-#define num_of_registers 16
-#define condition_mask 0xF000000000000000 // Bits 63-61
-#define condition_shift 60
-#define immediate_mask 0x800000000000000 // Bits 60
-#define immediate_shift 59
-#define opcode_mask 0x7F8000000000000 // Bits 59-52
-#define opcode_shift 51
-#define set_mask 0x4000000000000 // Bits 51
-#define set_shift 50
-#define register_num_mask 0x3C00000000000 // Bits 47-44
-#define register_num_shift 46
-#define register_dest_mask 0x3C0000000000 // Bits 43-41
-#define register_dest_shift 42
-#define opperand_2_mask 0xffffffff // Bits 31-0
+#include "vm_macros.h"
 
 // Instruction decoding variables
 extern struct {
@@ -45,5 +19,5 @@ extern uint64_t pc;
 extern uint32_t psr;
 
 // Functions
-void * VirtualMemToReal(uint32_t address);
-void white_pine(int argc, char **argv);
+extern void * VirtualMemToReal(uint32_t address);
+extern void white_pine(int argc, char **argv);
