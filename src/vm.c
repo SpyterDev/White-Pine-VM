e7d399b7d2c6a02c7789263d581df7e963b7299c
SpyterDev <spyterdev@gmail.com>
2024-08-30 01:14:04 -0400

    Deleted macros, added instructions

--- src/vm.c
+++ src/vm.c
@@ -26,6 +26,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "instr.h"
+#include "vm_macros.h"
 #include "rnapi.h"
 
 // Version stuff
@@ -33,38 +34,12 @@
 #define version_minor 0
 #define version_micro 1
 
-// Important macros
-typedef uint8_t * arithmetic_void_ptr;
-#define allocated_memory 53248
-#define stack_size 524288
-#define less_than 1
-#define greater_than 2
-#define equal_to 4
-#define all_cond 16
-#define real_address_mask 0xfff
-#define real_address_bits 12
-#define stack_mem_pages (stack_size >> real_address_bits)
-#define heap_mem_pages allocated_memory / real_address_bits
-#define num_of_registers 16
-#define condition_mask 0xF000000000000000 // Bits 63-61
-#define condition_shift 60
-#define immediate_mask 0x800000000000000 // Bits 60
-#define immediate_shift 59
-#define opcode_mask 0x7F8000000000000 // Bits 59-52
-#define opcode_shift 51
-#define set_mask 0x4000000000000 // Bits 51
-#define set_shift 50
-#define register_num_mask 0x3C00000000000 // Bits 47-44
-#define register_num_shift 46
-#define register_dest_mask 0x3C0000000000 // Bits 43-41
-#define register_dest_shift 42
-#define opperand_2_mask 0xffffffff // Bits 31-0
-
 // Registers
 uint32_t r[num_of_registers];
 void * sp = 0;
 arithmetic_void_ptr stack_mem_address = 0;
 uint64_t pc = 0;
+uint64_t lr = 0;
 uint32_t psr = 0xffffffff;
 
 // Instruction decoding variables
@@ -146,21 +121,29 @@ void execute(unsigned char opcode) {
     case b:
         pc = decoded_flags.imm;
         break;
+    case bl:
+        lr = pc;
+        pc = decoded_flags.imm;
+        break;
+    case ret:
+        pc = lr;
+        lr = 0;
+        break;
     case ldr:
         r[decoded_flags.rD] = *(uint32_t*) VirtualMemToReal(decoded_flags.imm);
         break;
     case str:
         *(uint32_t *)VirtualMemToReal(decoded_flags.imm) = r[decoded_flags.rD];
         break;
-    case rnpi:
-        if (decoded_flags.rM < number_of_rnapi_functions) runtime_native_api_functions[decoded_flags.rM]();
+    case rnapi:
+        if (decoded_flags.rM < runtime_native_api_functions.num_of_funcs) runtime_native_api_functions.func[decoded_flags.rM]();
         break;
     case cmp: 
         psr = psr&(~0xf);
         uint32_t compared_value = decoded_flags.immM ? decoded_flags.imm : r[decoded_flags.rM];
-        if (r[decoded_flags.rN] == compared_value) psr | equal_to;
-        else if (r[decoded_flags.rN] > compared_value) psr | less_than;
-        else if (r[decoded_flags.rN] < compared_value) psr | greater_than;
+        if (r[decoded_flags.rN] == compared_value) psr |= equal_to;
+        else if (r[decoded_flags.rN] > compared_value) psr |= less_than;
+        else if (r[decoded_flags.rN] < compared_value) psr |= greater_than;
         break;
     default:
         break;
